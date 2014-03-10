2c30222fd43f704dfaa6d0d396d7714541cf9b7f
Andy L Jones <andyjones.ed@gmail.com>
2014-03-10 23:00:26 +0000

    Initial commit.
     - TfL timetables are in src/main/resources
     - Schema and xjc output from them are in schema/

---
+++ src/main/java/foo/App.java
@@ -0,0 +1,14 @@
+package foo;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+public class App 
+{    
+    public static void parse() 
+    {
+
+        
+    }
+}
---
+++ src/test/java/foo/AppTest.java
@@ -0,0 +1,14 @@
+package foo;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+@RunWith(JUnit4.class)
+public class AppTest 
+{
+    @Test
+    public void testParse()
+    {
+    }
+}
