a046b5ce516a791740c52aedea0568278b79f8e6
Andy L Jones <andyjones.ed@gmail.com>
2014-03-13 11:54:36 +0000

    Implemented TransitGraphUpdater.

---
+++ src/main/java/io/github/andyljones/commutes_and_rents/TransitGraphUpdater.java
@@ -0,0 +1,65 @@
+package io.github.andyljones.commutes_and_rents;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Adds new cliques to a graph, using the specified edge weight for the new edges.
+ * @author andy
+ *
+ */
+public class TransitGraphUpdater {
+
+    private final long newEdgeWeight;
+    
+    public TransitGraphUpdater(long newEdgeWeight)
+    {
+        this.newEdgeWeight = newEdgeWeight;
+    }
+    
+    /**
+     * Builds new cliques in a graph.
+     * @param graph Graph that will be altered.
+     * @param cliquesToAdd Cliques to add.
+     */
+    public void update(Map<String, Node> graph, Collection<Collection<String>> cliquesToAdd)
+    {
+        for (Collection<String> clique : cliquesToAdd)
+        {
+            addClique(graph, clique);
+        }
+    }
+    
+    public void addClique(Map<String, Node> graph, Collection<String> cliqueToAdd)
+    {
+        for (String nodeRef : cliqueToAdd)
+        {
+            Collection<String> otherNodeRefs = cliqueToAdd.stream().filter(ref -> ref != nodeRef).collect(Collectors.toList());
+            
+            if (!otherNodeRefs.isEmpty())
+            {
+                addEdges(graph, nodeRef, otherNodeRefs);
+            }
+        }
+    }
+
+    private void addEdges(Map<String, Node> graph, String originRef, Collection<String> destinationNodeRefs)
+    {
+        for (String otherNodeRef : destinationNodeRefs)
+        {
+            addEdge(graph, originRef, otherNodeRef);
+        }
+    }
+
+    private void addEdge(Map<String, Node> graph, String originNodeRef, String destinationNodeRef) 
+    {
+        Node originNode = graph.get(originNodeRef);
+        Node destinationNode = graph.get(destinationNodeRef);
+        
+        OutEdge newEdge = new OutEdge(destinationNode, newEdgeWeight);
+        
+        originNode.getNeighbours().add(newEdge);
+    }
+    
+}
